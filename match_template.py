551396128754c4a48e26a78db4bf290863e7216f
Jakub Bednarski <bednar1230@gmail.com>
2024-10-30 17:11:38 +0100

    cleaner presentation

--- match_template.py
+++ match_template.py
@@ -11,6 +11,23 @@ for template_file in os.listdir(template_dir):
         templates[template_name] = cv2.imread(template_path)
 
 
+name_to_sign = {
+    'Ace': 'A',
+    'King': 'K',
+    'Queen': 'Q',
+    'Jack': 'J',
+    'Ten': '10',
+    'Nine': '9',
+    'Eight': '8',
+    'Seven': '7',
+    'Six': '6',
+    'Five': '5',
+    'Four': '4',
+    'Three': '3',
+    'Two': '2',
+}
+
+
 def match_template(card_image, template_image):
     # Convert the template to grayscale (if needed)
     template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
