49c47f5eb0031d20d069f8ce9a60575134fdcf96
Jakub Bednarski <bednar1230@gmail.com>
2024-10-30 20:33:13 +0100

    something added to points but needs to include A 1 or 11

--- match_template.py
+++ match_template.py
@@ -27,6 +27,22 @@ name_to_sign = {
     'Two': '2',
 }
 
+name_to_points = {
+    'Ace': 11,
+    'King': 10,
+    'Queen': 10,
+    'Jack': 10,
+    'Ten': 10,
+    'Nine': 9,
+    'Eight': 8,
+    'Seven': 7,
+    'Six': 6,
+    'Five': 5,
+    'Four': 4,
+    'Three': 3,
+    'Two': 2,
+}
+
 
 def match_template(card_image, template_image):
     # Convert the template to grayscale (if needed)
